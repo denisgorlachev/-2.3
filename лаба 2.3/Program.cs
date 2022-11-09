257eb798ac90648b459d1dd1a7896a59468f9611
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:42:00 +0200

    Добавьте файлы проекта.

---
+++ лаба 2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Горлачев базовый уровень
+Console.WriteLine("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+double c = double.Parse(Console.ReadLine());
+double min = a;
+if (b < min)
+    min = b;
+if (c < min)
+    min = c;
+Console.WriteLine("min: " + min);
